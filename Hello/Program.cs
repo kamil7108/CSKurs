21a407b78cc75b7df947f03acd82b4b0182e488e
Kamil Machulik <kamilmachulik7108@gmail.com>
2020-02-25 14:08:39 +0100

    v.0

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Hello
 {
     class Program
     {
+        private const string Value = "Siema2";
+
         static void Main(string[] args)
         {
+            foreach (var item in args)
+            {
+            System.Console.WriteLine(item);
+            }
+               
+            
         }
     }
 }
